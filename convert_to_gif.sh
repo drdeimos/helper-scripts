5a265148bff6910d606d1ecefa711eae4d576b09
Mikhail Nosov <mikhail.nosov@flant.com>
2018-01-03 10:33:09 +0300

    Add: video to gif convert script

---
+++ convert_to_gif.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Author: Mikhail Nosov (drdeimosnn@gmail.com)
+# Convert video to HQ gif
+# convert_to_gif.sh <source_video> <destination_gif> <start_time> <duration>
+# Example usage:
+# convert_to_gif.sh source_video.mp4 out_animation.gif "00:00:14" "00:00:10"
+
+INFILE=${1}
+OUTFILE=${2}
+PALETE="/tmp/$(makepasswd --chars=20).png"
+STARTPOS=${3:-"00:00:00"}
+DURATION=${4:-"NONE"}
+
+function convert {
+  INFILE=${1}
+  if [ ${DURATION} != "NONE" ];then
+    ffmpeg -ss ${STARTPOS} -t ${DURATION} -i ${INFILE} -vf \
+      fps=15,scale=320:-1:flags=lanczos,palettegen ${PALETE}
+    ffmpeg -ss ${STARTPOS} -t ${DURATION} -i ${INFILE} -i ${PALETE} \
+      -filter_complex "fps=15,scale=400:-1:flags=lanczos[x];[x][1:v]paletteuse" ${OUTFILE}
+  else
+    ffmpeg -ss ${STARTPOS} -i ${INFILE} -vf \
+      fps=15,scale=320:-1:flags=lanczos,palettegen ${PALETE}
+    ffmpeg -ss ${STARTPOS} -i ${INFILE} -i ${PALETE} \
+      -filter_complex "fps=15,scale=400:-1:flags=lanczos[x];[x][1:v]paletteuse" ${OUTFILE}
+  fi
+}
+
+
+if [ $# -gt 1 ];then
+  convert ${INFILE}
+else
+  echo $#
+  echo "Usage: $0 <input_file> <output_file>"
+  exit
+fi
+
+echo "Output file: ${OUTFILE}"
+rm -v ${PALETE}
