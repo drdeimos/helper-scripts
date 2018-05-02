c34ea0e9440b199c2db63dbbb4dbb340fc6717db
Mikhail Nosov <mikhail.nosov@flant.com>
2018-05-03 00:42:56 +0300

    Add: pkg-config for build deps

--- polybar_update.sh
+++ polybar_update.sh
@@ -17,7 +17,7 @@ RELEASE=$(date '+%Y%m%d%H%M%S')
 ### BUILD BSPWM ###
 BUILD_DIR=$(mktemp -d)
 REQUIRES="libxcb-xkb1 libxcb-randr0 xcb-proto libxcb-ewmh2 libxcb-icccm4 python-xcbgen libiw30"
-BUILD_DEPS="cmake libxcb-xkb-dev libxcb-randr0-dev libxcb-render-util0-dev libxcb-icccm4-dev libxcb-ewmh-dev python-xcbgen libiw-dev libasound2-dev libcurl4-openssl-dev"
+BUILD_DEPS="cmake libxcb-xkb-dev libxcb-randr0-dev libxcb-render-util0-dev libxcb-icccm4-dev libxcb-ewmh-dev python-xcbgen libiw-dev libasound2-dev libcurl4-openssl-dev pkg-config"
 DESCRIPTION="Polybar aims to help users build beautiful and highly customizable status bars for their desktop environment, without the need of having a black belt in shell scripting."
 REPO="https://github.com/jaagr/polybar.git"
 
