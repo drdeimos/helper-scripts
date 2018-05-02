cbd90d099108ff97f98ec54c1a665fcf6fb486f3
Mikhail Nosov <mikhail.nosov@flant.com>
2018-05-03 01:16:22 +0300

    Fix: missing deps

--- polybar_update.sh
+++ polybar_update.sh
@@ -16,8 +16,8 @@ RELEASE=$(date '+%Y%m%d%H%M%S')
 
 ### BUILD BSPWM ###
 BUILD_DIR=$(mktemp -d)
-REQUIRES="libxcb-xkb1 libxcb-randr0 xcb-proto libxcb-ewmh2 libxcb-icccm4 python-xcbgen libiw30 libxcb-image0"
-BUILD_DEPS="cmake libxcb-xkb-dev libxcb-randr0-dev libxcb-render-util0-dev libxcb-icccm4-dev libxcb-ewmh-dev python-xcbgen libiw-dev libasound2-dev libcurl4-openssl-dev pkg-config libcairo2-dev libxcb-image0-dev"
+REQUIRES="libxcb-xkb1 libxcb-randr0 xcb-proto libxcb-ewmh2 libxcb-icccm4 python-xcbgen libiw30 libxcb-image0 libxcb-util1 libxcb-cursor0"
+BUILD_DEPS="cmake libxcb-xkb-dev libxcb-randr0-dev libxcb-render-util0-dev libxcb-icccm4-dev libxcb-ewmh-dev python-xcbgen libiw-dev libasound2-dev libcurl4-openssl-dev pkg-config libcairo2-dev libxcb-image0-dev fontconfig git libxcb-util-dev libpulse-dev libxcb-cursor-dev"
 DESCRIPTION="Polybar aims to help users build beautiful and highly customizable status bars for their desktop environment, without the need of having a black belt in shell scripting."
 REPO="https://github.com/jaagr/polybar.git"
 
